36072047060035626505e73c8ae911888f043e5b
Boburt <66620107+Boburt@users.noreply.github.com>
2021-01-02 15:14:32 +0500

    исправлен auth

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -85,7 +85,7 @@ export default {
         this.authError = ''
         this.isAuthLoading = true
         const { data } = await this.$axios.post(
-          'https://gavali.fungeek.net/rest/1/l6yxn7688v0l6ewl/myuser.admin.auth',
+          'https://mapp.gavali.uz/rest/1/l6yxn7688v0l6ewl/myuser.admin.auth',
           {
             login: this.formUsername,
             password: btoa(this.formPassword),
